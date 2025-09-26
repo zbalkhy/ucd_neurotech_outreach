003bc460d9bc212aee080d59780e1096e5e8aae6
zbalkhy <zbalkhy@ucdavis.edu>
2025-09-26 15:30:13 -0700

    adding data

--- src/filterClass.py
+++ src/filterClass.py
@@ -20,7 +20,7 @@ class FilterClass():
             cutoff_freq = np.array(self.filters['frequency'][i])
             filter_order = self.filters['order'][i]
             normalized_cutoff = cutoff_freq / (.5 * fs)
-            b, a = signal.butter(filter_order, normalized_cutoff, btype=filter_type, analog=True)
+            b, a = signal.butter(filter_order, normalized_cutoff, btype=filter_type, analog=False)
             data = signal.filtfilt(b, a, data)
         return data
     
