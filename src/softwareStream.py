c0f48c712178db5f88c25a6640325066a302f4a6
Andy Vo <qwvo@ucdavis.edu>
2025-11-21 23:11:42 -0800

    Datastream now transposes the queue and outputs a np array, software stream outputs a 2d array, rows is time column is channel number

--- src/softwareStream.py
+++ src/softwareStream.py
@@ -15,10 +15,15 @@ class SoftwareStream(DataStream):
         t=0
         try:
             while not self.shutdown_event.is_set():
-                self.data.append(15*np.sin(0.01*np.pi*t) + 
+                waveform_ch1 = (15*np.sin(0.01*np.pi*t) + 
                                  12*np.sin(0.03*np.pi*t) + 
                                  1*np.sin(0.15*np.pi*t) + 
                                  11*np.sin(0.07*np.pi*t))
+                waveform_ch2 = (3*np.sin(0.02*np.pi*t) + 
+                                 7*np.sin(0.1*np.pi*t) + 
+                                 8*np.sin(0.04*np.pi*t) + 
+                                 4*np.sin(0.08*np.pi*t))
+                self.data.append([waveform_ch1, waveform_ch2])
                 t += 1
                 sleep(0.004)
         except:
