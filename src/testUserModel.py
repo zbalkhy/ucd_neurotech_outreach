75427e39c8938cb66a722798077d38e69e8e4667
DevinSeb <dkhunseb100@gmail.com>
2025-11-29 17:35:01 -0800

    Created test case for remove_stream_by_name

--- src/testUserModel.py
+++ src/testUserModel.py
@@ -11,7 +11,7 @@ class TestUserModel(unittest.TestCase):
     def test_get_stream(self):
         stream_name1 = "stream1"
         stream_name2 = 'stream2'
-
+        #create some streams and add to data_streams_dictionary
         data_stream1 = DataStream(stream_name1, StreamType.DEVICE, 100)
         data_stream2 = DataStream(stream_name2, StreamType.SOFTWARE, 100)
         self.user_model.add_stream(data_stream1)
@@ -39,23 +39,38 @@ class TestUserModel(unittest.TestCase):
         self.user_model.notify.assert_any_call(None, EventType.DEVICELISTUPDATE)
         self.assertEqual(self.user_model.notify.call_count,2)
 
-# User model declares:
-# if a user overrides a stream with the same name, the program loses...
-# track of the old stream thread and cannot quit
-    def test_add_stream_with_same_name(self):
-        self.user_model.notify = MagicMock()
-        stream_name = "stream"
-        data_stream1 = DataStream(stream_name, StreamType.SOFTWARE, 100)
-        #want to magicmock stream1 to mimic it, then test to see if stop() is called
-        data_stream2 = DataStream(stream_name, StreamType.DEVICE, 100)
-        self.user_model.add_stream(data_stream1)
-        self.user_model.add_stream(data_stream2)
+# # User model declares:
+# # if a user overrides a stream with the same name, the program loses track of the old stream thread and cannot quit
+# # test whether the implemented check works
+#     def test_add_stream_with_same_name(self):
+#         self.user_model.notify = MagicMock()
+#         stream_name = "stream"
+#         old_stream = DataStream(stream_name, StreamType.SOFTWARE, 100)
+#         new_stream = DataStream(stream_name, StreamType.DEVICE, 100)
+#         self.user_model.add_stream(old_stream)
+#         self.user_model.add_stream(new_stream)
 
-        self.assertIs(self.user_model.data_streams[stream_name], data_stream2)
+#         self.assertNotIn(self.user_model.data_streams[stream_name], data_stream2)
         
+    def test_remove_stream_by_name(self):
+        #remove_stream_by_name has a shutdown event, checks if stream is alive, and a join event
+        # Check that these are accounted for
+        stream_name = "stream"
+        data_stream1 = MagicMock(spec=DataStream)
+        data_stream1.shutdown_event = MagicMock()
+        data_stream1.is_alive = MagicMock(return_value = True)
+        data_stream1.join = MagicMock()
+        self.user_model.data_streams[stream_name] = data_stream1
+
+        self.user_model.remove_stream_by_name(stream_name)
+        self.assertNotIn(stream_name, self.user_model.data_streams)
+        data_stream1.shutdown_event.set.assert_called_once()
+        data_stream1.is_alive.assert_called_once()
+        data_stream1.join.assert_called_once()
 
 
-    #def test_add_stream(self):
+
+
         
 if __name__ == '__main__':
     unittest.main()
