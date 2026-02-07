9a7bfc2b1fbfa86938f73c7a33dce8507c1418b8
angelajzhou <155860768+angelajmzhou@users.noreply.github.com>
2026-02-07 00:15:39 -0800

    add test for function saving

--- src/Tests/testSave.py
+++ src/Tests/testSave.py
@@ -10,6 +10,7 @@ from Classes.filterClass import FilterClass
 from Classes.featureClass import FeatureClass, FeatureType
 from Classes.classifierClass import Classifier, MODEL_SAVE_DIR
 
+#super basic unit tests just to make sure stuff ends up dumped to json, no tkinter
 @pytest.fixture
 def setup_teardown():
     if os.path.exists(USER_SAVE_PATH):
@@ -27,11 +28,8 @@ def setup_teardown():
 def test_save_empty(setup_teardown):
     user = UserModel()
     save_model = SaveModel()
-    
     save_model.dump(user)
-    
     loaded_user = save_model.load()
-    
     assert isinstance(loaded_user, UserModel)
     assert len(loaded_user.data_streams) == 0
     assert len(loaded_user.filters) == 0
@@ -43,12 +41,9 @@ def test_save_stream(setup_teardown):
     user = UserModel()
     stream = DataStream("TestStream", StreamType.SOFTWARE)
     user.add_stream(stream)
-    
     save_model = SaveModel()
     save_model.dump(user)
-    
     loaded_user = save_model.load()
-    
     assert "TestStream" in loaded_user.data_streams
     loaded_stream = loaded_user.data_streams["TestStream"]
     assert loaded_stream.stream_name == "TestStream"
@@ -57,12 +52,9 @@ def test_save_stream(setup_teardown):
 def test_save_filter(setup_teardown):
     user = UserModel()
     user.add_filter("TestFilter", "lowpass", 4, 30)
-    
     save_model = SaveModel()
     save_model.dump(user)
-    
     loaded_user = save_model.load()
-    
     assert "TestFilter" in loaded_user.filters
     loaded_filter = loaded_user.filters["TestFilter"]
     assert loaded_filter.filter_name == "TestFilter"
@@ -74,12 +66,9 @@ def test_save_dataset(setup_teardown):
     user = UserModel()
     data = np.array([[1, 2, 3], [4, 5, 6]])
     user.add_dataset("TestDataset", data)
-    
     save_model = SaveModel()
     save_model.dump(user)
-    
     loaded_user = save_model.load()
-    
     assert "TestDataset" in loaded_user.data_sets
     np.testing.assert_array_equal(loaded_user.data_sets["TestDataset"], data)
 
@@ -98,29 +87,35 @@ def test_save_feature(setup_teardown):
 def test_save_classifier(setup_teardown):
     user = UserModel()
     classifier = Classifier()
-    
     dataset0 = np.random.rand(10, 5)
     dataset1 = np.random.rand(10, 5)
     classifier.add_dataset(0, "set0", dataset0)
     classifier.add_dataset(1, "set1", dataset1)
-    
     feature = FeatureClass(FeatureType.BETA)
     classifier.add_feature(feature)
     flt = FilterClass("CommonFilter")
     classifier.add_filter(flt)
-
     user.add_classifier("TestClassifier", classifier)
-    
     save_model = SaveModel()
     save_model.dump(user)
-    
     loaded_user = save_model.load()
-    
     assert "TestClassifier" in loaded_user.classifiers
     loaded_classifier = loaded_user.classifiers["TestClassifier"]
-    
     assert "set0" in loaded_classifier.label0_datasets
     np.testing.assert_array_equal(loaded_classifier.label0_datasets["set0"], dataset0)
-    
     assert len(loaded_classifier.features) == 1
     assert loaded_classifier.features[0].type == FeatureType.BETA
+
+def test_save_functions(setup_teardown):
+    user = UserModel()
+    user.functions["func1"] = "def func1(x): return x + 1"
+    user.functions["func2"] = "def func2(y): return y * 2"
+
+    save_model = SaveModel()
+    save_model.dump(user)
+    loaded_user = save_model.load()
+
+    assert "func1" in loaded_user.functions
+    assert "func2" in loaded_user.functions
+    assert loaded_user.functions["func1"] == "def func1(x): return x + 1"
+    assert loaded_user.functions["func2"] == "def func2(y): return y * 2"
